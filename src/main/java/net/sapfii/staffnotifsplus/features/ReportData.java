cddcfbc35df6321f44a1e9b050bf65b586c16515
sapfii <159391078+sapfii@users.noreply.github.com>
2025-04-10 03:46:29 -0400

    made text wrap & made reportdata class to contain report data

---
+++ src/main/java/net/sapfii/staffnotifsplus/features/ReportData.java
@@ -0,0 +1,10 @@
+package net.sapfii.staffnotifsplus.features;
+
+import net.minecraft.text.Text;
+
+import java.util.List;
+
+public class ReportData {
+    public int x, y, opacity, bgOpacity;
+    public List<Text> texts;
+}
--- src/main/java/net/sapfii/staffnotifsplus/features/ReportDisplayFeature.java
+++ src/main/java/net/sapfii/staffnotifsplus/features/ReportDisplayFeature.java
@@ -37,102 +37,123 @@ public class ReportDisplayFeature implements RenderedFeature, PacketListeningFea
     }
 
     int lerp(int a, int b, float t) {
-        int result = (int) Math.floor(a + (b - a) * t);
+        int result = (int) Math.floor(a + ((b - a) * t));
         if (result == a && result != b) {
             return a + 1;
         }
-        return (int) result;
+        return result;
     }
 
-    // bg color = 0x88000000
+    ArrayList<Text> wrap(Text text, int maxWidth) {
+        TextRenderer textRenderer = Flint.getClient().textRenderer;
+        String textString = text.getString();
+
+        ArrayList<Text> wrappedTexts = new ArrayList<>();
+        StringBuilder progress = new StringBuilder();
+        StringBuilder oldProgress = new StringBuilder();
+
+        for (int i = 0; i < textString.split("\\s+").length; ++i) {
+
+            String word = textString.split("\\s+")[i];
+            oldProgress = new StringBuilder(progress.toString());
+            if (progress.isEmpty()) {
+                progress = new StringBuilder(word);
+            } else {
+                progress.append(" ").append(word);
+            }
+            System.out.println(progress.toString().split("\\s+").length);
+            if (textString.endsWith(word)) {
+                wrappedTexts.add(Text.literal(progress.toString()));
+                return wrappedTexts;
+            }
+            if (textRenderer.getWidth(progress.toString()) > maxWidth && textRenderer.getWidth(word) < maxWidth) {
+                wrappedTexts.add(Text.literal(oldProgress.toString()));
+                progress = new StringBuilder(word);
+            }
+            if (textRenderer.getWidth(word) > maxWidth) {
+                wrappedTexts.add(Text.literal(oldProgress.toString()));
+                progress = new StringBuilder();
+                wrappedTexts.add(Text.literal(word));
+            }
+        }
+        return wrappedTexts;
+    }
+
 
     boolean clearReports = false;
 
-    HashMap<Text[], HashMap<String, Integer>> reportData = new HashMap<>(3);
-    ArrayList<Text[]> reports = new ArrayList<>(3);
+    ArrayList<ReportData> reports = new ArrayList<>();
     private static final Pattern INC_RPT_REGEX = Pattern.compile(
             "! Incoming Report \\((?<reporter>.+)\\)\\nOffender: (?<offender>.+)\\nOffense: (?<offense>.+)\\nLocation: (?<location>.+)"
     );
 
-
     @Override
     public void render(DrawContext draw, RenderTickCounter renderTickCounter) {
+        float delta = renderTickCounter.getTickDelta(true);
+
         if (keyBinding.isPressed() && !reports.isEmpty() && !clearReports) {
             clearReports = true;
             Flint.getUser().getPlayer().playSound(SoundEvent.of(Identifier.of("staffnotifsplus","report_dismiss")));
         }
         TextRenderer textRenderer = Flint.getClient().textRenderer;
-        float delta = renderTickCounter.getTickDelta(true);
 
         int maxLength = 0;
-
         int reportOffset = 0;
-        for (Text[] reportTexts : reports) {
+        for (int i = 0; i < reports.size(); ++i) {
+            ReportData report = reports.get(i);
+
             boolean render = true;
 
-            for (Text reportText : reportTexts) {
+            for (Text reportText : report.texts) {
                 int textLength = textRenderer.getWidth(reportText);
                 if (textLength > maxLength) {
                     maxLength = textLength;
                 }
             }
+            report.x = lerp(report.x, reportOffset, 0.15F * delta);
 
-            int x = reportData.get(reportTexts).get("x");
-            int y = reportData.get(reportTexts).get("y");
-            int expectedX = reportOffset;
-            reportData.get(reportTexts).put("x", lerp(x, expectedX, 0.15F * delta));
-            x = reportData.get(reportTexts).get("x");
-
-            int bgOpacity = reportData.get(reportTexts).get("bgOpacity");
-            int opacity = reportData.get(reportTexts).get("opacity");
+            int bgOpacity = report.bgOpacity;
+            int opacity = report.opacity;
             int bgColor = 0x66000000;
             int color = 0xFFFFFFFF;
 
-
-            if (reports.size() == 3) {
-                if (reports.get(2) == reportTexts) {
-                    int bgAlpha = lerp(bgOpacity, 0, 0.2F * delta);
-                    int alpha = lerp(opacity, 0, 0.15F * delta);
-                    bgColor = (bgAlpha << 24);
-                    color = (alpha << 24) | color & 0x00FFFFFF;
-                    if (alpha <= 10) {
-                        render = false;
-                    }
-                    reportData.get(reportTexts).put("bgOpacity", bgAlpha);
-                    reportData.get(reportTexts).put("opacity", alpha);
+            if (i == 2) {
+                int bgAlpha = lerp(bgOpacity, 0, 0.2F * delta);
+                int alpha = lerp(opacity, 0, 0.15F * delta);
+                bgColor = (bgAlpha << 24);
+                color = (alpha << 24) | color & 0x00FFFFFF;
+                if (alpha <= 10) {
+                    render = false;
                 }
+                report.bgOpacity = bgAlpha;
+                report.opacity = alpha;
             }
 
-            if (clearReports) {
-                if (reports.size() == 3 && (reports.get(2) == reportTexts)) {
-                    break;
+            if (clearReports && i < 2) {
+                int bgAlpha = lerp(bgOpacity, 0, 0.2F * delta);
+                int alpha = lerp(opacity, 0, 0.15F * delta);
+                bgColor = (bgAlpha << 24);
+                color = (alpha << 24) | color & 0x00FFFFFF;
+                if (alpha <= 10) {
+                    reports.clear();
+                    clearReports = false;
+                    return;
                 } else {
-                    int bgAlpha = lerp(bgOpacity, 0, 0.2F * delta);
-                    int alpha = lerp(opacity, 0, 0.15F * delta);
-                    bgColor = (bgAlpha << 24);
-                    color = (alpha << 24) | color & 0x00FFFFFF;
-                    if (alpha <= 10) {
-                        reports.clear();
-                        clearReports = false;
-                        return;
-                    } else {
-                        reportData.get(reportTexts).put("bgOpacity", bgAlpha);
-                        reportData.get(reportTexts).put("opacity", alpha);
-                    }
+                    report.opacity = alpha;
+                    report.bgOpacity = bgAlpha;
                 }
             }
 
-            if (render && !reports.isEmpty()) {
-                draw.fill(x, y-5, (10 + maxLength)+x, y+5+(textRenderer.fontHeight*4), bgColor);
-                int txtI = 0;
-                for (Text reportText : reportTexts) {
+            if (render) {
+                draw.fill(report.x, report.y-5, (10 + maxLength)+report.x, report.y+5+(textRenderer.fontHeight*report.texts.size()), bgColor);
+                for (int txtI = 0; txtI < report.texts.size(); ++txtI) {
+                    Text reportText = report.texts.get(txtI);
                     draw.drawTextWithShadow(
                             textRenderer,
                             reportText,
-                            x+5, y+(textRenderer.fontHeight*txtI),
+                            report.x+5, report.y+(textRenderer.fontHeight*txtI),
                             color
                     );
-                    txtI += 1;
                 }
                 reportOffset += 15 + maxLength;
             }
@@ -150,32 +171,34 @@ public class ReportDisplayFeature implements RenderedFeature, PacketListeningFea
 
         Matcher matcher = INC_RPT_REGEX.matcher(string);
         if (matcher.find()) {
-            Text[] reportTexts = new Text[4];
+            ArrayList<Text> reportTexts = new ArrayList<>();
 
-            reportTexts[0] = Text.literal("! ").styled(style -> {
-                    style = style.withColor(0xFB5454);
-                    style = style.withBold(true);
-                    return style;})
-                .append(Text.literal("Incoming Report ").styled(style -> {
-                    style = style.withColor(0xa7a7a7);
-                    style = style.withBold(false);
-                    return style;}))
-                .append(Text.literal("(" + matcher.group("reporter") + ")").styled(style -> {
-                    style = style.withColor(0x545454);
-                    style = style.withBold(false);
-                    return style;}));
+            reportTexts.add(Text.literal("! ").styled(style -> {
+                        style = style.withColor(0xFB5454);
+                        style = style.withBold(true);
+                        return style;})
+                    .append(Text.literal("Incoming Report ").styled(style -> {
+                        style = style.withColor(0xa7a7a7);
+                        style = style.withBold(false);
+                        return style;}))
+                    .append(Text.literal("(" + matcher.group("reporter") + ")").styled(style -> {
+                        style = style.withColor(0x545454);
+                        style = style.withBold(false);
+                        return style;})));
 
-            reportTexts[1] = Text.literal("Offender: ").withColor(0xFF545454)
-                    .append(Text.literal(matcher.group("offender")).withColor(0xFFFFFFFF));
+            reportTexts.add(Text.literal("Offender: ").withColor(0xFF545454)
+                    .append(Text.literal(matcher.group("offender")).withColor(0xFFFFFFFF)));
 
-            reportTexts[2] = Text.literal("Offense: ").withColor(0xFF545454)
-                    .append(Text.literal(matcher.group("offense")).withColor(0xFFFFFFFF));
+            ArrayList<Text> offenseLines = new ArrayList<>();
 
-            reportTexts[3] = Text.literal("Location: ").withColor(0xFF545454)
-                    .append(Text.literal(matcher.group("location")).withColor(0xFFFFFFFF));
+            ArrayList<Text> wrappedOffenseLines = wrap(Text.literal(matcher.group("offense")), 200);
+            offenseLines.add(Text.literal("Offense: ").withColor(0xFF545454).append(Text.literal(wrappedOffenseLines.getFirst().getString()).withColor(0xFFFFFFFF)));
+            wrappedOffenseLines.removeFirst();
+            offenseLines.addAll(wrappedOffenseLines);
+            reportTexts.addAll(2, offenseLines);
 
-            reports.addFirst(reportTexts);
-            reportData.put(reportTexts, new HashMap<>());
+            reportTexts.add(Text.literal("Location: ").withColor(0xFF545454)
+                    .append(Text.literal(matcher.group("location")).withColor(0xFFFFFFFF)));
 
             int maxLength = 0;
 
@@ -186,12 +209,14 @@ public class ReportDisplayFeature implements RenderedFeature, PacketListeningFea
                 }
             }
 
-            int x = -15 - maxLength;
-            int y = 30;
-            reportData.get(reportTexts).put("x", x);
-            reportData.get(reportTexts).put("y", y);
-            reportData.get(reportTexts).put("opacity", 255);
-            reportData.get(reportTexts).put("bgOpacity", 102);
+            ReportData data = new ReportData();
+            data.x = -15 - maxLength;
+            data.y = 30;
+            data.opacity = 255;
+            data.bgOpacity = 102;
+            data.texts = reportTexts;
+            reports.addFirst(data);
+
             if (reports.size() > 3) {
                 reports.remove(3);
             }
